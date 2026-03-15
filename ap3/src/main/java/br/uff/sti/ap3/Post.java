63df07e107ee54e0d4dab73f83c996d0d3ab1c95
mvpcortes <marcoscortes@id.uff.br>
2026-03-15 16:36:39 -0300

    por mvpcortes: últimos ajustes AP3.

--- ap3/src/main/java/br/uff/sti/ap3/Post.java
+++ ap3/src/main/java/br/uff/sti/ap3/Post.java
@@ -2,7 +2,6 @@ package br.uff.sti.ap3;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.With;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.MappedCollection;
@@ -21,8 +20,4 @@ public record Post(
         @MappedCollection(idColumn = "post_id", keyColumn = "ordem")
         List<Tag> tags
 ) {
-
-        private static List<Tag> strToTag(List<String> strTags) {
-            return strTags.stream().map(Tag::new).toList();
-    }
 }
